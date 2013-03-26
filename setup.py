a723ea87d19f716781855adcefa2bb46dd9b2dca
Alexey Lavrenuke (load testing) <direvius@yandex-team.ru>
2013-03-26 19:03:46 +0400

    Format for pylint

--- setup.py
+++ setup.py
@@ -1,26 +1,34 @@
 from setuptools import setup, find_packages
-import sys, os
 
 version = '0.1.0'
 
-setup(name='yhc',
-      version=version,
-      description="Phrase classifier",
-      long_description="""\
+setup(
+    name='yhc',
+    version=version,
+    description="Phrase classifier",
+    long_description="""\
 Phrase classifiers collection and common interface to them""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='library classification machine-learning natural-language-processing',
-      author='Alexey Lavrenuke',
-      author_email='direvius@yandex-team.ru',
-      url='',
-      license='LGPLv3',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='library classification machine-learning natural-language-processing',
+    author='Alexey Lavrenuke, Ilya Melnikov',
+    author_email='direvius@yandex-team.ru, imeln@yandex-team.ru',
+    url='',
+    license='LGPLv3',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+)
